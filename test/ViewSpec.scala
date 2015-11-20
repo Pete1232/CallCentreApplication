3a26f2439dc99d3b355de39c76928cb9eff43728
Pete1232 <pete1232@gmail.com>
2015-11-20 16:28:14 +0000

    Added test for newcustomer view

---
+++ test/ViewSpec.scala
@@ -0,0 +1,40 @@
+import models.entities.Customer
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+  * Created by pnewman on 20/11/2015.
+  */
+class ViewSpec extends UnitSpec{
+
+  def testNewCustomer: Unit ={
+    val testForm = Form(mapping(
+      "cid" -> longNumber,
+      "username" -> nonEmptyText,
+      "email" -> email)(Customer.apply)(Customer.unapply)
+    )
+    lazy val html = views.html.newcustomer(testForm)
+
+    "The newcustomer view" should "render an html page" in {
+      running(new FakeApplication()) {
+        contentType(html) should be("text/html")
+      }
+    }
+
+    it should "contain a form field for customer ID" in{
+      contentAsString(html) should include ("cid_field")
+    }
+
+    it should "contain a form field for customer username" in{
+      contentAsString(html) should include ("username_field")
+    }
+
+    it should "contain a form field for customer email" in{
+      contentAsString(html) should include ("email_field")
+    }
+  }
+  testNewCustomer
+}
